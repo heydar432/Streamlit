9ec4abe71a0b0cbc5ad714f0f2d03590150c010d
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-02-18 15:48:51 +0400

    Update ln.project.py

--- Kia_Models_Price_Prediction/ln.project.py
+++ Kia_Models_Price_Prediction/ln.project.py
@@ -5,7 +5,7 @@ import requests
 import io
 import warnings
 warnings.filterwarnings("ignore")
-pip install xgboost==<version>
+pip install xgboost
 
 # Function to load a file from a URL
 def load_file_from_url(url):
