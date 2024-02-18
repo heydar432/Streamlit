00c79a019b756aa818553d1192221c4ac7a30f78
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-02-18 16:45:20 +0400

    Update ln.project.py

--- Kia_Models_Price_Prediction/ln.project.py
+++ Kia_Models_Price_Prediction/ln.project.py
@@ -69,6 +69,7 @@ oil_type_mapping = {
     'Benzin': 'Petrol',
     'Dizel': 'Diesel',
     'Hibrid': 'Hybrid',
+    'Elektro': 'Electric',
     'Qaz': 'Gas',
     'Plug-in Hibrid': 'Plug-in Hybrid'
 }
