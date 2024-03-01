8759bdad93e517ce2cee51ec12666763f1504c53
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-03-02 01:07:02 +0400

    Update eng_dict_app.py

--- Eng_dict_app/eng_dict_app.py
+++ Eng_dict_app/eng_dict_app.py
@@ -132,9 +132,9 @@ else:
     if st.session_state.incorrect_answers:
         st.markdown("<h2 style='text-align: center; color: red;'>Review the incorrect answers:</h2>", unsafe_allow_html=True)
         for term, defs, pron in st.session_state.incorrect_answers:
-            st.markdown(f"<h3 style='text-align: left; color: red;'>Term: {term}</h3>", unsafe_allow_html=True)
-            st.markdown(f"<h3 style='text-align: left; color: yellow;'>Definition: {defs}</h3>", unsafe_allow_html=True)
-            st.markdown(f"<h3 style='text-align: left; color: yellow;'>Pronunciation: {pron}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left; color: red;'>Term: {term}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left; color: yellow;'>Definition: {defs}</h3>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left; color: yellow;'>Pronunciation: {pron}</h3>", unsafe_allow_html=True)
 
 
     # Option to restart the quiz
