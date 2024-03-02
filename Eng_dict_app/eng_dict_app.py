941cd027e0c9174f8c4e55cb46233af9f9ef4364
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-03-02 18:02:05 +0400

    Update eng_dict_app.py

--- Eng_dict_app/eng_dict_app.py
+++ Eng_dict_app/eng_dict_app.py
@@ -133,10 +133,10 @@ else:
         st.markdown("<h2 style='text-align: center; color: red;'>Review the incorrect answers:</h2>", unsafe_allow_html=True)
         for term, defs, pron, user_ans in st.session_state.incorrect_answers:
             st.markdown(f"<h4 style='text-align: left; color: black; font-weight: bold;'>Term: <span style='color: red;'>{term}</span></h4>", unsafe_allow_html=True)
-            st.markdown(f"<h4 style='text-align: left; color: black; font-size: 20px;'> 📝✔️ <span style='color: red; font-style: italic;'>{defs}</span></h4>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left; color: black; font-size: 20px;'> 📖✔️ <span style='color: red; font-style: italic;'>{defs}</span></h4>", unsafe_allow_html=True)
             st.markdown(f"<h4 style='text-align: left; color: black; font-size: 20px;'> 📣✔️ <span style='color: red; font-style: italic;'> [ {pron} ]</span></h4>", unsafe_allow_html=True)
             # Display the user's answer
-            st.markdown(f"<h4 style='text-align: left; color: black; font-size: 18px;'> ❌ Your answer: <span style='color: blue;'>'{user_ans}'</span></h4>", unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left; color: black; font-size: 18px;'> ✍️❌  <span style='color: blue;'>'{user_ans}'</span></h4>", unsafe_allow_html=True)
 
     # Option to restart the quiz
     if st.button("Restart Quiz"):
