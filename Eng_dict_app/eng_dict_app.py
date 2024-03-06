61c96095840a8ca35da88c056d8a268a8efa4aeb
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-03-06 12:47:32 +0400

    Update eng_dict_app.py

--- Eng_dict_app/eng_dict_app.py
+++ Eng_dict_app/eng_dict_app.py
@@ -5,6 +5,12 @@ import random
 from datetime import datetime
 import time
 
+# Display the resized image aligned to the center horizontally using CSS styling
+st.markdown(
+    f'<div style="display: flex; justify-content: center;"><img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/087cab2d-fdd1-4960-96d4-99b8e6587e97/dgovrr-f7618dc4-6e94-4ce1-8bb7-6023cdeb4da1.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzA4N2NhYjJkLWZkZDEtNDk2MC05NmQ0LTk5YjhlNjU4N2U5N1wvZGdvdnJyLWY3NjE4ZGM0LTZlOTQtNGNlMS04YmI3LTYwMjNjZGViNGRhMS5qcGcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ._pUfJfXa6QbLKihXmEVpkhycCB6mNLdTsWhoaDfdoDg" style="width: 200px;"></div>',
+    unsafe_allow_html=True
+)
+
 # Initialize session state variables for the timer if they don't exist
 if 'timer_start' not in st.session_state:
     st.session_state.timer_start = None
