b49c1fae0bc159b03fbf9e4f33c17e4031113c85
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-03-06 18:16:17 +0400

    Update eng_dict_app.py

--- Eng_dict_app/eng_dict_app.py
+++ Eng_dict_app/eng_dict_app.py
@@ -20,31 +20,18 @@ dataset_choice = st.radio(
     ('uşaqlar_1', 'Heydar_mixed_eng', '799_words', '54_words')
 )
 
+# Mapping of dataset choices to their respective Google Sheet IDs and Sheet names
+datasets = {
+    'uşaqlar_1': ('1MvSa70n992Fs0jmS1vEjux4x4NzT6KaO', 'Sheet1'),
+    'Heydar_mixed_eng': ('1SxNKWXeXQzE2WHj1sQ_sMRO5KqW6Jb-y', 'Sheet1'),
+    '799_words': ('15ByeHMRtCroYD1zN2Tymlecq5A-xHPE-', 'Sheet1'),
+    '54_words': ('1u7howTZIMTL9REa7SIX3-J3i73bIABSH', 'Sheet1'),
+}
+
 # Use the chosen dataset for the quiz
-if dataset_choice == 'uşaqlar_1':
-    eng_sheet_id = '1MvSa70n992Fs0jmS1vEjux4x4NzT6KaO'
-    eng_sheet_name = 'Sheet1'  # Replace with the actual sheet name if different
-    eng_google_sheet_url = f'https://docs.google.com/spreadsheets/d/{eng_sheet_id}/gviz/tq?tqx=out:csv&sheet={eng_sheet_name}'
-    df = pd.read_csv(eng_google_sheet_url)
-    
-elif dataset_choice == 'Heydar_mixed_eng':
-    # Convert Google Sheets URL to a CSV export format for 'Heydar_mixed_eng'
-    heydar_sheet_id = '1SxNKWXeXQzE2WHj1sQ_sMRO5KqW6Jb-y'
-    heydar_sheet_name = 'Sheet1'  # Replace with the actual sheet name if different
-    heydar_google_sheet_url = f'https://docs.google.com/spreadsheets/d/{heydar_sheet_id}/gviz/tq?tqx=out:csv&sheet={heydar_sheet_name}'
-    df = pd.read_csv(heydar_google_sheet_url)
-elif dataset_choice == '799_words':
-    # Convert Google Sheets URL to a CSV export format for '799_words'
-    words_799_sheet_id = '15ByeHMRtCroYD1zN2Tymlecq5A-xHPE-'
-    words_799_sheet_name = 'Sheet1'  # Replace with the actual sheet name if different
-    words_799_google_sheet_url = f'https://docs.google.com/spreadsheets/d/{words_799_sheet_id}/gviz/tq?tqx=out:csv&sheet={words_799_sheet_name}'
-    df = pd.read_csv(words_799_google_sheet_url)
-else:
-    # Convert Google Sheets URL to a CSV export format for '54_words'
-    words_54_sheet_id = '1u7howTZIMTL9REa7SIX3-J3i73bIABSH'
-    words_54_sheet_name = 'Sheet1'  # Replace with the actual sheet name if different
-    words_54_google_sheet_url = f'https://docs.google.com/spreadsheets/d/{words_54_sheet_id}/gviz/tq?tqx=out:csv&sheet={words_54_sheet_name}'
-    df = pd.read_csv(words_54_google_sheet_url)
+sheet_id, sheet_name = datasets[dataset_choice]
+google_sheet_url = f'https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}'
+df = pd.read_csv(google_sheet_url)
 
 
 
