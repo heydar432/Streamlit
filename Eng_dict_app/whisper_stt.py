cdbd7f565987f2872276c5406e4e133e2983e972
Heydar Zulfugar <65925995+heydar432@users.noreply.github.com>
2024-08-29 19:31:58 +0400

    Update whisper_stt.py

--- Eng_dict_app/whisper_stt.py
+++ Eng_dict_app/whisper_stt.py
@@ -1,30 +1,12 @@
-import sounddevice as sd
-import scipy.io.wavfile as wav
 import whisper
 
-def record_and_transcribe(duration=5, sample_rate=44100, model_size="small", language="en"):
-    # Record audio from the microphone
-    print("Recording...")
-    audio = sd.rec(int(duration * sample_rate), samplerate=sample_rate, channels=1, dtype='int16')
-    sd.wait()  # Wait until the recording is finished
-    print("Recording finished")
+# Load the Whisper model
+model = whisper.load_model("small")
 
-    # Save the recording to a WAV file
-    wav_file = "microphone_audio.wav"
-    wav.write(wav_file, sample_rate, audio)
+voice_path = r"/home/heydar/Desktop/Data_Science/Minapy/transcribe using Whisper/Whisper_detailed_tests/recordings_for_comparison using app_2_for_comparison.py and app_2_config_for_comparison.yaml/full_audio_20240808_155642.wav"
+# Transcribe the audio file
+result = model.transcribe(voice_path, language = 'en')
 
-    # Load the Whisper model
-    model = whisper.load_model(model_size, device="cpu")
-
-    # Transcribe the recorded audio
-    result = model.transcribe(wav_file, language=language)
-
-    # Return the transcription text
-    return result['text']
-
-# Use the transcription in your main script
-transcription = record_and_transcribe()
-
-# You can now use 'transcription' wherever you need in your main script
-print("Transcription:", transcription)
+# Print the transcription
+print(result['text'])
 
